3c7f1ff05cc635101d7262b5368e35bec1eefbdf
BalbesX <62122956+BalbesX@users.noreply.github.com>
2020-03-30 18:48:18 +0300

    123

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// let sum = 0;
+
+// while(true){
+//     const num = prompt("??")
+
+//     if (num == "="){
+//         break;
+//     } 
+//    else{
+//         sum = sum + Number(num); 
+//     }  
+        
+//     }
+
+// console.log(sum)
+
+var sex = prompt('')
+for (var i = 1; i <= sex; i++){
+    console.log("")
+}
